54669fc5f606c7974434204e22fc020e62e09128
Konstantin Shcherbinin <shcherbinin.k@tecomgroup.ru>
2023-09-15 21:58:03 +0300

    Patterns project. Implemented: 1. Builder 2. Singleton

---
+++ Patterns/Builder.cs
@@ -0,0 +1,84 @@
+﻿namespace Patterns
+{
+    /// <summary>
+    /// Abstract builder. Determines algorithms which constructs result entity Product
+    /// </summary>
+    public abstract class Builder
+    {
+        public abstract void BuildPartA(string partA);
+        public abstract void BuildPartB(string partB);
+        public abstract void BuildPartC(string partC);
+        public abstract Product Build();
+    }
+
+    /// <summary>
+    /// This class provides data, but not constructing them. Functionality of filling 
+    /// parts object (part A, part B, part C) with concrete configuration are provided 
+    /// to side classes.
+    /// </summary>
+    public class Product
+    {
+        List<string> parts = new List<string>(); 
+        
+        public void Add(string part)
+        {
+            parts.Add(part);
+        }
+
+        public IReadOnlyList<string> GetParts() => parts.AsReadOnly();
+    }
+
+    /// <summary>
+    /// Concrete builder. Implements abstract builder class
+    /// </summary>
+    public class ConcreteBuilder : Builder 
+    {
+        Product product = new Product();
+
+        public override void BuildPartA(string partA)
+        {
+            product.Add(partA);
+        }
+
+        public override void BuildPartB(string partB)
+        {
+            product.Add(partB);
+        }
+
+        public override void BuildPartC(string partC)
+        {
+            product.Add(partC);
+        }
+
+        public override Product Build()
+        {
+            return product;
+        }
+    }
+
+    /// <summary>
+    /// Class, which methods are responsible to construct concrete realization
+    /// in concrete builder.
+    /// </summary>
+    public class Director
+    {
+        private Builder _builder;
+        public Director(Builder builder)
+        {
+            _builder = builder;
+        }
+
+        public void ConstructBAC()
+        {
+            _builder.BuildPartB("PartB");
+            _builder.BuildPartA("PartA");
+            _builder.BuildPartC("PartC");
+        }
+
+        public void ConstructBC()
+        {
+            _builder.BuildPartB("PartB");
+            _builder.BuildPartC("PartC");
+        }
+    }
+}
---
+++ Patterns/Singleton.cs
@@ -0,0 +1,46 @@
+﻿namespace Patterns
+{
+    public class Singleton
+    {
+        private static Singleton __instance;
+
+        /// <summary>
+        /// Classic realization of Singleton pattern
+        /// </summary>    
+        private Singleton() { }
+
+        public Singleton GetInstance()
+        {
+            if (__instance == null)
+                __instance = new Singleton();
+            
+            return __instance;
+        }
+    }
+    
+    // TODO: сделать generic реализацию, наподобие DI контейнера
+    public class GenericSingleton<T, V> 
+        where T : class
+        where V : class
+    {
+        private Dictionary<T, V> _instances = new Dictionary<T,V>();
+
+        public GenericSingleton()
+        {
+
+        }
+
+        public void BindInstance(T inputInterface, V classTemplate)
+        {
+            if (!inputInterface.GetType().IsInterface)
+                throw new ArgumentException("Invalid input");
+
+
+        }
+
+        public V GetInstance(T requestedInterface)
+        {
+            return _instances[requestedInterface];
+        }
+    }
+}
