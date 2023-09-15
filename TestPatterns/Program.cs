98246ab73a01ddcd09c3214d59257aa69713557a
Konstantin Shcherbinin <shcherbinin.k@tecomgroup.ru>
2023-09-15 21:58:43 +0300

    Project to test patterns

---
+++ TestPatterns/Program.cs
@@ -0,0 +1,28 @@
+﻿using Patterns;
+
+namespace TestPatterns
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var product = Builder();
+
+            foreach (var item in product.GetParts())
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+          
+        private static Product Builder()
+        {
+            var concreteBuilder = new ConcreteBuilder();
+            
+            var director = new Director(concreteBuilder);
+            director.ConstructBAC();
+
+            return concreteBuilder.Build();
+        }
+    }
+}
