5bceb2b2884f9f7dc920eba5eab6fbacc7e5df8a
Konstantin Shcherbinin <shcherbinin.k@tecomgroup.ru>
2023-09-20 22:34:17 +0300

    Class helper. Represent function which need to integrate

---
+++ Algorithms/Helpers/NumericalIntegration/IntegratedFunction.cs
@@ -0,0 +1,17 @@
+﻿namespace NumericAlgorithms.Helpers
+{
+    public record IntegratedFunction
+    {
+        public Func<float, float> Function { get; private set; }
+        public float XMin { get; private set; }
+        public float XMax { get; private set; }
+        public float Segment { get => XMax - XMin; }
+
+        public IntegratedFunction(Func<float, float> function, float xMin, float xMax) 
+        {
+            Function = function;
+            XMin = xMin;
+            XMax = xMax;
+        }
+    }
+}
